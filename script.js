8966246f6ce5af8e36c25fb16107499ce6dba129
Minhaz Ali Khan <136866102+MinhazAliKhan@users.noreply.github.com>
2023-09-08 02:13:59 -0500

    Responsive login Page using tailwind CSS

---
+++ script.js
@@ -0,0 +1,5 @@
+let mobile_icon=document.getElementById("mobile_icon");
+let main_manu=document.getElementById("main_menu");
+mobile_icon.addEventListener("click",()=>{
+    main_manu.classList.toggle('hidden');
+})
